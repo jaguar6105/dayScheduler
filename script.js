b1ef9b38a13e68a3da53b32a0bc73606d37a78a5
jaguar6105 <gilljoseph603@gmail.com>
2020-09-22 14:57:53 -0400

    add timeblocks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
 
 // initialize the dates
@@ -8,11 +8,27 @@ function initialize() {
     var d = new Date();
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     $("#currentDay").text(months[d.getMonth()] + " " + d.getDate());
-
+    createTimes();
 }
 
-
+//create timeblocks
 function createTimes() {
+    $.each( times, function(i, val) {
+
+        var timeblock = $("<div>");
+        timeblock.addClass("jumbotron jumbotron-fluid");
+
+        var time;
+        if(i < 3) {
+            time = val + "AM";
+        }
+        else {
+            time = val + "PM";
+        }
+
+        $(".container").append(timeblock);
+        timeblock.append("<p>" + time + "</p>");
+      });
 
 }
 
