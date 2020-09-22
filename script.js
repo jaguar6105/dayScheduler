5d349fa1c27885d9ec6c4e9e6ca434d87a2789b1
jaguar6105 <gilljoseph603@gmail.com>
2020-09-22 15:13:14 -0400

    add color change for time

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-
+var times = [12, 1, 2, 3, 4,5,6,7,8, 9, 10, 11, 12, 1, 2, 3, 4, 5,6,7,8,9,10,11];
+var d = new Date();
+var actualTime = d.getHours();
 
 // initialize the dates
 function initialize() {
 
-    var d = new Date();
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     $("#currentDay").text(months[d.getMonth()] + " " + d.getDate());
     createTimes();
@@ -13,22 +13,35 @@ function initialize() {
 
 //create timeblocks
 function createTimes() {
-    $.each( times, function(i, val) {
+    for(var i = 9; i < 18; i++) {
+
+        var val = times[i];
 
         var timeblock = $("<div>");
-        timeblock.addClass("jumbotron jumbotron-fluid");
-
         var time;
-        if(i < 3) {
+        if(i < 12) {
             time = val + "AM";
         }
         else {
             time = val + "PM";
         }
 
+        timeblock.addClass("jumbotron jumbotron-fluid " + val);
+
+        if(i > actualTime) {
+            timeblock.css("background-color", "lightgreen");
+        }
+        else if (i < actualTime) {
+            timeblock.css("background-color", "gray");
+        }
+        else {
+            timeblock.css("background-color", "red");
+        }
+
+        
         $(".container").append(timeblock);
         timeblock.append("<p>" + time + "</p>");
-      });
+      }
 
 }
 
